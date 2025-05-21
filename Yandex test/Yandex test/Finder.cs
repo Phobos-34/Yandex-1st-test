f75468f3d6369e61e88b7f4dccd4ceb1dfa514cf
Valiantmax <30309910+Phobos-34@users.noreply.github.com>
2025-05-21 14:45:37 +0300

    F

---
+++ Yandex test/Yandex test/Finder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yandex_test
+{
+    static public class Finder
+    {
+        public static Tuple<int, int> FindTwoSmallestSum(int[] array)
+        {
+            if (array.Length < 2)
+                throw new ArgumentException("Массив должен содержать как минимум два элемента"); //Проверка на размер массива
+
+            int min1 = int.MaxValue;
+            int min2 = int.MaxValue;
+
+            foreach (int num in array)
+            {
+                if (num < min1)
+                {
+                    min2 = min1;
+                    min1 = num; //Если текущий элемент меньше min1, он становится новым min1, а старое значение min1 становится min2
+                }
+                else if (num < min2)
+                {
+                    min2 = num; //Иначе, если он меньше min2, обновляется только min2
+                }
+            }
+
+            return Tuple.Create(min1, min2);
+        }
+    }
+}
---
+++ Yandex test/Yandex test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Yandex_test;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 3, 5, 1, 9, -2, 0 }; //Создаем массив
+
+        var result = Finder.FindTwoSmallestSum(arr);
+        Console.WriteLine($"Два числа с наименьшей суммой: {result.Item1} и {result.Item2}");
+        Console.WriteLine($"Сумма: {result.Item1 + result.Item2}");
+    }
+}
+
+//Временная сложность: O(n)
